b4964fbf8d4fd99fb8e3de67f2cf6acafc5cf4a1
msx98 <git@lx.ax>
2026-03-21 17:00:01 +0000

    change trivalent pass store to gnome keyring

--- specs/trivalent/init.sh
+++ specs/trivalent/init.sh
@@ -1,5 +1,6 @@
 exec /usr/lib64/trivalent/trivalent \
     --ozone-platform-hint=auto \
     --ozone-platform=wayland \
+    --password-store=gnome-keyring \
     --class=trivalent-$INSTANCE \
     "$@"
